8471e0b95248f1331ca8011a91b20efd53ba806b
Zaaraah <iraharcega@yahoo.com.ph>
2014-05-23 16:44:24 +0800

    edit

---
+++ db/migrate/20140523084222_create_sicknesses.rb
@@ -0,0 +1,11 @@
+class CreateSicknesses < ActiveRecord::Migration
+  def change
+    create_table :sicknesses do |t|
+      t.string :name
+      t.string :symptoms
+      t.string :descriptions
+
+      t.timestamps
+    end
+  end
+end
